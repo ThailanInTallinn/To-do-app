84e8cdfa624a26bffce259ddf70dad003c4aa87c
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-03-25 20:20:09 +0200

    feat: returns all persisted tasks

--- src/main/java/com/gouveacorp/enhancedtodo/service/TodoService.java
+++ src/main/java/com/gouveacorp/enhancedtodo/service/TodoService.java
@@ -1,4 +1,41 @@
 package com.gouveacorp.enhancedtodo.service;
 
+import com.gouveacorp.enhancedtodo.model.DTO.TodoDtoResponse;
+import com.gouveacorp.enhancedtodo.model.Todo;
+import com.gouveacorp.enhancedtodo.model.DTO.TodoDto;
+import com.gouveacorp.enhancedtodo.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TodoService {
+    private final TodoRepository todoRepository;
+
+    public Todo createTodo(TodoDto todoDto){
+        if(todoDto != null){
+            LocalDateTime now = LocalDateTime.now();
+            Todo todoObj = new Todo();
+            todoObj.dtoToEntity(todoDto);
+            todoObj.setCreatedAt(now);
+            todoObj.setUpdatedAt(now);
+            return todoRepository.save(todoObj);
+        } else {
+            throw new IllegalArgumentException("To-do cannot be null.");
+        }
+
+    }
+
+    public List<TodoDtoResponse> getTodos(){
+        /*List<Todo> listTodos = null;
+        for(Todo todo : todoRepository.findAll()){
+            listTodos.add(todo);
+        }
+        return listTodos;*/
+
+        return todoRepository.findAll().stream().toList();
+    }
 }
