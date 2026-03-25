eec2dcc07e9e7130b5aaa55d5c8c26f2069eac01
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-03-25 20:07:32 +0200

    adds base get endpoint

--- src/main/java/com/gouveacorp/enhancedtodo/controller/TodoController.java
+++ src/main/java/com/gouveacorp/enhancedtodo/controller/TodoController.java
@@ -1,4 +1,28 @@
 package com.gouveacorp.enhancedtodo.controller;
 
+
+import com.gouveacorp.enhancedtodo.model.Todo;
+import com.gouveacorp.enhancedtodo.model.DTO.TodoDto;
+import com.gouveacorp.enhancedtodo.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TodoController {
+    private final TodoService todoService;
+
+    @PostMapping
+    public Todo createTodo(@RequestBody TodoDto todo) {
+        return todoService.createTodo(todo);
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos(){
+        return todoService.getTodos();
+    }
 }
