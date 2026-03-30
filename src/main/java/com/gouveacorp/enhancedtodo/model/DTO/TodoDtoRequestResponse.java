ada0aaca5305277bd4774e519e6d280ad4ab240e
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-03-30 22:21:42 +0300

    Makes response dto handle also post requests

---
+++ src/main/java/com/gouveacorp/enhancedtodo/model/DTO/TodoDtoRequestResponse.java
@@ -0,0 +1,17 @@
+package com.gouveacorp.enhancedtodo.model.DTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoDtoRequestResponse {
+    private Long id;
+
+    private String task;
+
+    private Boolean completed;
+}
