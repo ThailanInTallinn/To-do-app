f83451a7980fafd47a51e9b6426b579461caf719
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-03-30 22:14:38 +0300

    Creates an endpoint for tasks editing
    It makes possible to edit/change the status of previously created tasks.

--- src/main/java/com/gouveacorp/enhancedtodo/service/TodoService.java
+++ src/main/java/com/gouveacorp/enhancedtodo/service/TodoService.java
@@ -1,6 +1,6 @@
 package com.gouveacorp.enhancedtodo.service;
 
-import com.gouveacorp.enhancedtodo.model.DTO.TodoDtoResponse;
+import com.gouveacorp.enhancedtodo.model.DTO.TodoDtoRequestResponse;
 import com.gouveacorp.enhancedtodo.model.Todo;
 import com.gouveacorp.enhancedtodo.model.DTO.TodoDto;
 import com.gouveacorp.enhancedtodo.repository.TodoRepository;
@@ -29,11 +29,24 @@ public class TodoService {
 
     }
 
-    public List<TodoDtoResponse> getTodos(){
-        
+    public List<TodoDtoRequestResponse> getTodos(){
+
         return todoRepository.findAll()
                 .stream()
                 .map(td -> td.entityToResponseDto(td))
                 .toList();
+    }
+
+    public void editTodo(Long id, TodoDtoRequestResponse dto) throws IllegalArgumentException{
+        try{
+            Todo oldTodo = todoRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("Todo with id " + id + " does not exist."));
+            oldTodo.setTask(dto.getTask());
+            oldTodo.setCompleted(dto.getCompleted());
+            todoRepository.save(oldTodo);
+        } catch(IllegalArgumentException e){
+            throw new RuntimeException("Failed to update todo with id " + id, e);
+        }
+
     }
 }
