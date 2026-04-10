936e51033ece8442a039ed365214316700b1e1a7
Thailan Gouvêa de Menezes <thailangm@icloud.com>
2026-04-10 21:21:11 +0300

    throws new exception type for not found todo

--- src/main/java/com/gouveacorp/enhancedtodo/service/TodoService.java
+++ src/main/java/com/gouveacorp/enhancedtodo/service/TodoService.java
@@ -1,10 +1,12 @@
 package com.gouveacorp.enhancedtodo.service;
 
-import com.gouveacorp.enhancedtodo.model.DTO.TodoDtoRequestResponse;
+import com.gouveacorp.enhancedtodo.TodoNotExistByIdException;
+import com.gouveacorp.enhancedtodo.model.DTO.TodoRequestResponseDto;
 import com.gouveacorp.enhancedtodo.model.Todo;
 import com.gouveacorp.enhancedtodo.model.DTO.TodoDto;
 import com.gouveacorp.enhancedtodo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -29,7 +31,7 @@ public class TodoService {
 
     }
 
-    public List<TodoDtoRequestResponse> getTodos(){
+    public List<TodoRequestResponseDto> getTodos() {
 
         return todoRepository.findAll()
                 .stream()
@@ -37,14 +39,15 @@ public class TodoService {
                 .toList();
     }
 
-    public void editTodo(Long id, TodoDtoRequestResponse dto) throws IllegalArgumentException{
+    @SneakyThrows
+    public void editTodo(Long id, TodoRequestResponseDto dto) {
         try{
             Todo oldTodo = todoRepository.findById(id)
-                    .orElseThrow(() -> new IllegalArgumentException("Todo with id " + id + " does not exist."));
+                    .orElseThrow(() -> new TodoNotExistByIdException("Todo with id " + id + " does not exist."));
             oldTodo.setTask(dto.getTask());
             oldTodo.setCompleted(dto.getCompleted());
             todoRepository.save(oldTodo);
-        } catch(IllegalArgumentException e){
+        } catch(TodoNotExistByIdException e){
             throw new RuntimeException("Failed to update todo with id " + id, e);
         }
 
